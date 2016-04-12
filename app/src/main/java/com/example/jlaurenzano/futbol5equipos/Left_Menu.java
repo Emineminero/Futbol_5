6f6c66e7beb0cc2b62cfb4f719ee66505bb13cbd
jlaurenzano <jlaurenzano@altimetrik.com>
2016-04-12 16:12:50 -0300

    changed from Blank activity to Left Bar Activity

--- app/src/main/java/com/example/jlaurenzano/futbol5equipos/Left_Menu.java
+++ app/src/main/java/com/example/jlaurenzano/futbol5equipos/Left_Menu.java
@@ -1,40 +1,52 @@
 package com.example.jlaurenzano.futbol5equipos;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import com.android.volley.*;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class Left_Menu extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     ArrayList<String> players = new ArrayList<String>();
     ArrayList<String> firstTeam = new ArrayList<String>();
     ArrayList<String> secondTeam = new ArrayList<String>();
@@ -45,14 +57,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_left__menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         //If we don't request focus of the button, then the first thing that will
         //appear in the app will be the huge and annoying Keyboard
         Button button = (Button)findViewById(R.id.armarCuadros);
+        button.performClick();
         button.requestFocus();
-
     }
-
     public void buttonOnClick(View v){
         firstTeam = new ArrayList<>();
         secondTeam = new ArrayList<>();
@@ -86,10 +108,10 @@ public class MainActivity extends AppCompatActivity {
             firstTeam.add(players.get(i).toString());
         }
 
-       for(int i = 5; i < players.size(); i++){
-           secondTeam.add(players.get(i).toString());
-       }
-        ListView  team1=(ListView) findViewById(R.id.team1);
+        for(int i = 5; i < players.size(); i++){
+            secondTeam.add(players.get(i).toString());
+        }
+        ListView team1=(ListView) findViewById(R.id.team1);
         ListView  team2=(ListView) findViewById(R.id.team2);
         adapter1 = null;
         adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, firstTeam);
@@ -150,9 +172,9 @@ public class MainActivity extends AppCompatActivity {
         ){
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-               Map<String, String> map = new HashMap<>();
-               map.put("equipo", "fabio");
-               map.put("equipos", "fabiolo");
+                Map<String, String> map = new HashMap<>();
+                map.put("equipo", "fabio");
+                map.put("equipos", "fabiolo");
 
                 return map;
             }
@@ -179,13 +201,66 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void toggleOnClick(View v){
-        ToggleButton shouldUpload = (ToggleButton)findViewById(R.id.uploadToServer);
-        if(shouldUpload.isChecked()){
+    public void toggleOnClick(MenuItem item){
+        SpannableString s = new SpannableString("Subir a Servidor?");
+        if(!shouldUploadChecked){
             shouldUploadChecked = true;
+            s.setSpan(new ForegroundColorSpan(Color.GREEN), 0, s.length(), 0);
+            item.setIcon(R.drawable.ic_menu_tick);
         } else {
             shouldUploadChecked = false;
+            s.setSpan(new ForegroundColorSpan(Color.RED), 0, s.length(), 0);
+            item.setIcon(R.drawable.ic_menu_cross);
         }
 
+        item.setTitle(s);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.left__menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.uploadToServer){
+             toggleOnClick(item);
+
+         }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
